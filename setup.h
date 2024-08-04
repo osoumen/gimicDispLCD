2259b99dde11afed82c056d5de656b4914cb3420
osoumen <osoumen@picopicose.com>
2024-08-04 17:19:54 +0900

    ヘッダーファイル名の間違いを修正

--- setup.h
+++ setup.h
@@ -7,7 +7,7 @@
 // #include "user_config/m5coreS3.h"
 // #include "user_config/m5stack_basic.h"
 // #include "user_config/rpipico_ILI9341_parallel.h"
-// #include "user_config/rpipico_ILI9342_spi.h"
+// #include "user_config/rpipico_ILI9341_spi.h"
 #include "user_config/rpipico_PicoResTouch_2_8.h"
 
 #if defined(TP_XR_PIN_NO) && defined(TP_YD_PIN_NO) && defined(TP_XL_PIN_NO) && defined(TP_YU_PIN_NO)
